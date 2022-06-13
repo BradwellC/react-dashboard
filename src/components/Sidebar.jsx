02cba4b3a67849049be70c5ce3c3dc630bb7dc00
BradwellC <59516796+BradwellC@users.noreply.github.com>
2022-06-13 13:51:58 +0100

    v5 - Navbar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -10,7 +10,13 @@ import { links } from '../data/dummy'
 import { useStateContext } from '../context/ContextProvider'
 
 const Sidebar = () => {
-  const { activeMenu, setActiveMenu } = useStateContext()
+  const { activeMenu, setActiveMenu, screenSize } = useStateContext()
+
+  const handleCloseSidebar = () => {
+    if (activeMenu && screenSize < 900) {
+      setActiveMenu(false)
+    }
+  }
 
   const activeLink = 'flex items-center pl-4 pt-3 pb-2.5 rounded-lg text-white text-md m-2'
   const regularLink = 'flex items-center pl-4 pt-3 pb-2.5 rounded-lg text-md text-gray-700 dark:text-gray-200 dark:hover:text-black hover:bg-light-gray m-2'
@@ -19,7 +25,7 @@ const Sidebar = () => {
     <div className='ml-3 h-screen md:overflow-hidden overflow-auto pb-10'>
       {activeMenu && (<>
         <div className='flex justify-between items-center'>
-          <Link to='/' onClick={() => setActiveMenu(false)} className='items-center gap-3 ml-3 mt-4 flex text-xl tracking-tight font-extrabold dark:text-white text-slate-900' >
+          <Link to='/' onClick={ handleCloseSidebar } className='items-center gap-3 ml-3 mt-4 flex text-xl tracking-tight font-extrabold dark:text-white text-slate-900' >
             <SiShopware /> <span>Shoppy</span>
           </Link>
           <TooltipComponent content='Menu' position='BottomCenter'>
@@ -35,7 +41,7 @@ const Sidebar = () => {
                 {item.title}
               </p>
               {item.links.map((link) => (
-                <NavLink to={`/${link.name}`} key={link.name} onClick={() => {}} className={({ isActive }) => isActive ? activeLink : regularLink}>
+                <NavLink to={`/${link.name}`} key={link.name} onClick={ handleCloseSidebar } className={({ isActive }) => isActive ? activeLink : regularLink}>
                   {link.icon} 
                   <span className='capitalize'>
                     {link.name}
